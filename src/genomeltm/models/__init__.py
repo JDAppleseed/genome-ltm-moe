50c1d68c57fa4a8a49e679b3ac2b67f34ee89d39
JDAppleseed <davanzojonathan@gmail.com>
2026-01-20 00:20:30 -0800

    Refactor models/__init__.py to include imports and __all__

--- src/genomeltm/models/__init__.py
+++ src/genomeltm/models/__init__.py
@@ -1,1 +1,15 @@
-"""Placeholder module: models/__init__.py."""
+from .variant_effect import VariantEffectHead, VariantEffectOutput
+from .splicing import SplicingHead, SplicingOutput
+from .regulatory import RegulatoryHead, RegulatoryOutput
+from .sv import StructuralVariantHead, SVOutput
+from .phasing import PhasingHead, PhasingOutput
+from .reliability import ReliabilityHead, ReliabilityOutput
+
+__all__ = [
+    "VariantEffectHead", "VariantEffectOutput",
+    "SplicingHead", "SplicingOutput",
+    "RegulatoryHead", "RegulatoryOutput",
+    "StructuralVariantHead", "SVOutput",
+    "PhasingHead", "PhasingOutput",
+    "ReliabilityHead", "ReliabilityOutput",
+]
